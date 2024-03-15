2694e20a0b29175082ee028dda205ddf6a849a8d
mauricioatm20 <mauriciochasi@gmail.com>
2024-03-15 10:54:24 +0100

    finalizando mi primera aplicacion con django

--- mysite/urls.py
+++ mysite/urls.py
@@ -14,18 +14,12 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
-from django.contrib import admin
-from django.urls import path
 
 from django.contrib import admin
 from django.urls import include, path
 
 from polls import views
-
-app_name = 'polls'
 urlpatterns = [
-    path('', views.index, name='index'),
-    path('<int:question_id>/', views.detail, name='detail'),
-    path('<int:question_id>/results/', views.results, name='results'),
-    path('<int:question_id>/vote/', views.vote, name='vote'),
+    path('polls/', include('polls.urls')),
+    path('admin/', admin.site.urls),
 ]
